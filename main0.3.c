c526af906968a47d49c98c31ee20b2aeadcbf4b8
Roberta Angioni <93215385+Berthaji@users.noreply.github.com>
2021-12-14 17:26:03 +0100

    Processo astronave e areaGioco
    Versione con sprite per l'astronave

---
+++ main0.3.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <curses.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAXX 80
+#define MAXY 24
+
+void astro(int pipeout);
+void area(int pipein);
+
+struct posizione{
+	int x;
+	int y;
+	char c;
+};
+
+
+char sprite[4]={"|_|>"};
+
+//Si implementa il movimento principale dell'astronave sottoforma di sprite
+int main(){
+	int p[2];
+	int status;
+	initscr();
+	noecho();
+	keypad(stdscr,1);
+	curs_set(0);
+	pipe(p);
+	
+	int pidA;
+	int dimsprite=sizeof(sprite);
+	pidA=fork();
+
+		
+		if(pidA==0){
+			mvprintw(0,MAXY/2,"|_|>");
+			close(p[0]);
+			astro(p[1]);
+		} else {
+			close(p[1]);
+			area(p[0]);
+			wait(&status);
+			}
+			
+	kill(pidA,1);
+	endwin();
+	return 0;
+}
+
+
+void astro(int pipeout)
+{
+	struct posizione oggetto;
+	oggetto.x= 0;
+	oggetto.y= MAXY/2;
+	oggetto.c='+';
+	int dimsprite=sizeof(sprite);
+	
+	write(pipeout,&oggetto,sizeof(oggetto));
+	
+	
+	while(true){
+	int c= getch();
+	
+	switch(c){
+		case KEY_UP:
+		if(oggetto.y > 0) oggetto.y-=1; break;
+		
+		case KEY_DOWN: 
+		if(oggetto.y < MAXY-1) oggetto.y+=1; break;
+		
+		case 113: endwin(); exit(0);	//non funziona
+		}
+		
+	/*clear();
+	mvaddch(oggetto.y, oggetto.x, '|');
+	mvaddch(oggetto.y, (oggetto.x+dimsprite/4), '_');
+	mvaddch(oggetto.y, (oggetto.x+dimsprite/2), '|');
+	mvaddch(oggetto.y, (oggetto.x+dimsprite*3/4), '>');
+	refresh();*/
+	write(pipeout,&oggetto,sizeof(oggetto));
+	}
+}
+
+
+void area(int pipein)
+{	struct posizione oggetto, dato_letto;
+	int dimsprite=sizeof(sprite);
+	
+	while(true){
+		read(pipein,&dato_letto,sizeof(dato_letto));
+		clear();
+		oggetto=dato_letto;
+		mvaddch(oggetto.y, oggetto.x, '|');
+		mvaddch(oggetto.y, (oggetto.x+dimsprite/4), '_');
+		mvaddch(oggetto.y, (oggetto.x+dimsprite/2), '|');
+		mvaddch(oggetto.y, (oggetto.x+dimsprite*3/4), '>');
+		curs_set(0);
+		refresh();
+		
+	}
+
+}
